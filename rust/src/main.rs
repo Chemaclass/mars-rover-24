2daad98d0787f9f2f3f54825fe73ef88c0928c77
Chemaclass <chemaclass@outlook.es>
2024-03-08 18:55:55 +0100

    red: it_moves_1_position

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,25 @@
+enum Bearing {
+    N, S, E, W
+}
+struct Position {
+    x: i32,
+    y: i32,
+}
+struct MarsRover {
+    position: Position,
+    bearing: Bearing,
+}
+
+impl MarsRover {
+    fn new() -> Self {
+        MarsRover { 
+            position: Position{x: 0, y: 0},
+            bearing: Bearing::N, 
+        }
+    }
+    // fn move(move) 
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -6,9 +28,19 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use crate::{MarsRover, Position};
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_moves_1_position() {
+        let mars = MarsRover::new();
+        mars.move("M");
+        
+        assert_eq!(Position {x:0, y: 1} , mars.position());
     }
 }
