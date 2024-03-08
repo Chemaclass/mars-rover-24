b2ed299c3234c04a0bfe7ba26067595fe3e37c44
Chemaclass <chemaclass@outlook.es>
2024-03-08 18:38:26 +0100

    testing_example and hello world

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// -- Tests
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
